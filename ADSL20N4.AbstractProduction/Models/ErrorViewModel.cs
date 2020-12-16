ea8b20884cf04bdec142d650a68415a00718f935
fdbva <villela.felipe@gmail.com>
2020-12-16 20:39:44 -0300

    Add project files.

---
+++ ADSL20N4.AbstractProduction/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADSL20N4.AbstractProduction.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
